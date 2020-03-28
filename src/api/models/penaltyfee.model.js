b4f0d066020b203e937eb12937566b78f6f6c71e
CarlosG <cg94947@gmail.com>
2020-03-27 22:40:26 -0500

    'test'

--- src/api/models/penaltyfee.model.js
+++ src/api/models/penaltyfee.model.js
@@ -1,10 +1,12 @@
 const mongoose= require('mongoose');
+const Person=require('./person.model');
 const Schema = mongoose.Schema;
 
  const PenaltyfeeSchema=new Schema({
     price:{type: Number,required:true},
     date:{type:Date,default: Date.now(),required:false},
     vehicleplate:{type:String,required:true},
+    person:{type: Schema.Types.ObjectId,ref: Person,required:false}
 });
 
 
