66b111812c73df42b7cffc837f697beb09235373
CarlosG <cg94947@gmail.com>
2020-04-07 17:36:52 -0500

    Correciones en rutas de personas

--- src/api/routes/person.routes.js
+++ src/api/routes/person.routes.js
@@ -6,8 +6,8 @@ const Auth=require('../middlewares/auth');
 const router = new Router();
 
 
-router.get('/',Auth.isAuth,PersonController.getPerson );
-router.get('/:id',Auth.isAuth,PersonController.getPeople  );
+router.get('/',Auth.isAuth,PersonController.getPeople );
+router.get('/:id',Auth.isAuth,PersonController.getPerson);
 router.post('/',Auth.isAuth,PersonController.postPerson );
 router.delete('/:id',Auth.isAuth,PersonController.deletePerson );
 router.put('/:id',Auth.isAuth,PersonController.putPerson );
