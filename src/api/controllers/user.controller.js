e6ade87b26b84fdb45dbd1a3fbe263027ac21a41
carlosgarcia01 <cg94947@gmail.com>
2020-05-24 16:02:29 -0500

    test

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,103 +1,94 @@
+/* eslint-disable no-shadow */
+// eslint-disable-next-line no-unused-vars
+const path = require('path');
+
 const UserService = require('../../services/user.service');
 const User = require('../models/user.model');
-const path = require('path');
+
 module.exports = {
-  async getUsers(req, res, next) 
-  {
+  async getUsers(req, res, next) {
     const users = await UserService.getAllUsers();
-      
+
     return res.status(200).send({ users });
-    
   },
 
-  async getUser(req, res, next)
-   {
+  async getUser(req, res, next) {
     const { id } = req.params;
 
-    const user = await UserService.getUserById(id);
+    const user = await UserService.getUserByDocument(id);
 
     return res.status(200).send({ user });
   },
 
-  async postUser(req,res,next)
-  {
-    
-    let userReceived = new User({
-      document:req.body.document,
-      name :req.body.name,
-      lastname :req.body.lastname,
-      contact :req.body.contact,
-      occupation :req.body.occupation,
-      email :req.body.email,
-      password :req.body.password
+  async postUser(req, res, next) {
+    const userReceived = new User({
+      document: req.body.document,
+      name: req.body.name,
+      lastname: req.body.lastname,
+      contact: req.body.contact,
+      occupation: req.body.occupation,
+      email: req.body.email,
+      password: req.body.password,
     });
 
-    //console.log(userReceived);
+    // console.log(userReceived);
 
     const newUser = await UserService.postUser(userReceived);
 
     return res.status(200).send({ newUser });
   },
-  
 
-      async putUser(req,res)
-      {
-        const {id}=req.params;
-        let userReceived = req.body;
-        userUpdate= await UserService.putUser(id,userReceived);
+  async putUser(req, res) {
+    const { id } = req.params;
+    const userReceived = req.body;
 
-        return res.status(200).send({userUpdate});
-      },
+    const userUpdate = await UserService.putUser(id, userReceived);
 
-      async deleteUser(req,res)
-      {
-        const { id } = req.params;
-        UserService.deleteUser(id);
-        return res.status(200).send('Deleted user');
-      },
+    return res.status(200).send({ userUpdate });
+  },
 
-      async signUp (req, res) {
-        let user = new User({
-            document:req.body.document,
-            name :req.body.name,
-            lastname :req.body.lastname,
-            contact :req.body.contact,
-            occupation :req.body.occupation,
-            email :req.body.email,
-            password :req.body.password
-        })
-      //acá invoco el gravatar antes de que grabe el usuario
-        user.avatar = user.gravatar();
-        user.save(async err => {
-          if(err) return res.status(500).send({ message: 
-            `Error al crear el usuario: ${err}`})
-          let generatedToken = await UserService.createToken(user) 
-    
-          return res.status(200).send({ token: generatedToken})
-        })
-      },
-      async signIn (req, res) {
-        User.findOne({ email: req.body.email }, (err, user) => {
-             if (err) return res.status(500).send({ message: 
-               `Error al ingresar: ${err}` })
-             if (!user) return res.status(404).send({ message: 
-               `No existe el usuario: ${req.body.email}` })
-            
-             return user.comparePassword(req.body.password,
-              async  (err, isMatch) => {
-               if (err) return res.status(500).send(
-                 { message: `Error al ingresar: ${err}` })
-               if (!isMatch) return res.status(404).send(
-                 { message: `Error de contraseña: ${req.body.email}` })
-         
-               //req.user = user
-               let generatedToken = await UserService.createToken(user)
-               return res.status(200).send({ message: 
-                 'Te has logueado correctamente', 
-                 token: generatedToken})
-             });      
-           }).select('_id email password');
-       },
+  async deleteUser(req, res) {
+    const { id } = req.params;
 
+    UserService.deleteUser(id);
 
+    return res.status(200).send('Deleted user');
+  },
+
+  async signUp(req, res) {
+    const user = new User({
+      document: req.body.document,
+      name: req.body.name,
+      lastname: req.body.lastname,
+      contact: req.body.contact,
+      occupation: req.body.occupation,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // acá invoco el gravatar antes de que grabe el usuario
+    user.avatar = user.gravatar();
+    user.save(async err => {
+      if (err) return res.status(500).send({ message: `Error al crear el usuario: ${err}` });
+      const generatedToken = await UserService.createToken(user);
+
+      return res.status(200).send({ token: generatedToken });
+    });
+  },
+  async signIn(req, res) {
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (err) return res.status(500).send({ message: `Error al ingresar: ${err}` });
+      if (!user) return res.status(404).send({ message: `No existe el usuario: ${req.body.email}` });
+
+      return user.comparePassword(req.body.password, async (err, isMatch) => {
+        if (err) return res.status(500).send({ message: `Error al ingresar: ${err}` });
+        if (!isMatch) return res.status(404).send({ message: `Error de contraseña: ${req.body.email}` });
+
+        // req.user = user
+        const generatedToken = await UserService.createToken(user);
+
+        return res.status(200).send({ message: 'Te has logueado correctamente', token: generatedToken });
+      });
+    }).select('_id email password');
+  },
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,57 +1,45 @@
-'use strict'
-const mongoose = require('mongoose');
-const User = require('../api/models/user.model');
-const jwt = require('../utils/jwt')
+/* eslint-disable no-undef */
+/* eslint-disable no-param-reassign */
 const moment = require('moment');
-const bcrypt = require('bcrypt')
-const {
-  JWT
-} = require('../config');
+const bcrypt = require('bcrypt');
 
+const User = require('../api/models/user.model');
+const jwt = require('../utils/jwt');
 
 module.exports = {
   async getAllUsers() {
+    const users = await User.find();
 
-     const users = await User.find();
-    
-   
     return users;
-
-
   },
 
+  async getUserByDocument(id) {
+    const user = await User.find({ document: id });
 
-
-  async getUserById(id) {
-
-    const user = await User.findById(id);
+    // const user = await User.findById(id);
     return user;
   },
 
   async postUser(userreq) {
-    console.log(userreq);
     const newUser = await userreq.save();
+
     return newUser;
   },
 
   async putUser(id, userreq) {
- 
-    
-    var newuser = await User.findByIdAndUpdate(id, userreq);
+    const newuser = await User.findByIdAndUpdate(id, userreq);
 
-    if(userreq.password){
-      bcrypt.genSalt(10,(err, salt) => {
-        bcrypt.hash(userreq.password, salt, null,async(err, hash) => {
-          userreq.password = hash
-          newUser=await User.findByIdAndUpdate(id, userreq)
-          
-        })
+    if (userreq.password) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(userreq.password, salt, null, async (_err, hash) => {
+          userreq.password = hash;
+          newUser = await User.findByIdAndUpdate(id, userreq);
+        });
       });
     }
-      return newuser;
+
+    return newuser;
   },
-
-
 
   async deleteUser(id) {
     await User.findByIdAndDelete(id);
@@ -60,32 +48,33 @@ module.exports = {
   async createToken(user) {
     const payload = {
       sub: user._id,
-      iat: moment().unix()
-      // exp:  moment().add(accessTokenExpiryTime/60/60/24,'days').unix()            
-    }
-    return await jwt.generateToken(payload, payload)
+      iat: moment().unix(),
+      // exp:  moment().add(accessTokenExpiryTime/60/60/24,'days').unix()
+    };
+
+    return await jwt.generateToken(payload, payload);
   },
 
   async decodeToken(token) {
     const decoded = new Promise((resolve, reject) => {
       try {
-        const payload = jwt.getDecodedToken(token)
+        const payload = jwt.getDecodedToken(token);
 
         if (payload.exp <= moment().unix()) {
           reject({
             status: 401,
-            message: 'El token ha expirado'
-          })
+            message: 'El token ha expirado',
+          });
         }
-        resolve(payload.sub)
+        resolve(payload.sub);
       } catch (err) {
         reject({
           status: 500,
-          message: 'Token no válido'
-        })
+          message: 'Token no válido',
+        });
       }
-    })
+    });
 
-    return decoded
-  }
-}
+    return decoded;
+  },
+};
