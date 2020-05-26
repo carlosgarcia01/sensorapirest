affc1137e2237f65e01d9d4a61a7592405545fc2
carlosgarcia01 <cg94947@gmail.com>
2020-05-25 20:57:24 -0500

    register model updated

--- src/api/models/register.model.js
+++ src/api/models/register.model.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-
+var date = new Date();
+var fecha = date.getDate() + "/" + (date.getMonth() +1) + "/" + date.getFullYear()
 const registerSchema = new Schema({
   document: { type: String, required: true },
   description: { type: String, required: false, default: '' },
-  registerDate: { type: String, required: false, default: Date.now().toString() },
+  registerDate: { type: String, required: false, default: fecha },
 });
 
 module.exports = mongoose.model('register', registerSchema);
